fd932d183266fcc2bcbed5cd4bc62e679ea0936f
Chirag01496 <adonebuiltcon@gmail.com>
2025-09-14 14:28:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-   experimental: {
-    turbo: false,
-  },
+  swcMinify: true
 };
 
 export default nextConfig;
